f95a16d481e5dce3f7bc38e11108d90ff471bd15
Josh Nichols <josh.nichols.lives@gmail.com>
2017-04-21 15:58:36 -0600

    what

--- app.py
+++ app.py
@@ -1,10 +1,26 @@
-from flask import Flask
+from flask import Flask, request
 
 app = Flask(__name__)
 
 @app.route("/")
 def index():
     return "Hello world!"
+
+# API routes
+
+@app.route("/api/v1/data", methods=["GET", "POST"])
+def get_post_data():
+    if request.method == "GET":
+        # We're sending all of our data to the user!
+        return "GET request received!"
+
+    elif request.method == "POST":
+        # There's a new data point in town!
+        return "POST request received!"
+
+    else:
+        # The only two requests that we support are "GET" and "POST"
+        return "Unsupported HTTP request", 400
 
 if __name__ == "__main__":
     app.run()
